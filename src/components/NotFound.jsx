a201142e8d240efb710efaa9e71abab993aba679
Rocío Sosa Machado <85970786+rososamachado@users.noreply.github.com>
2021-12-19 19:13:48 -0300

    Add files via upload

---
+++ src/components/NotFound.jsx
@@ -0,0 +1,11 @@
+const NotFound = () => {
+    return (
+        <>
+            <h1>Ups... todavia esto no esta pronto</h1>
+
+            <h3>¡En breves estará disponible!</h3>
+        </>
+    )
+}
+
+export default NotFound
